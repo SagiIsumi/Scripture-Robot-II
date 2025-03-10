88177bf757f8789cd2a21ad92fef012e9a4cd8fe
SagiIsumi <saginomiyaisumi0924@gmail.com>
2025-03-10 15:58:40 +0800

    add offline service

--- GPTpackages/LocalModel.py
+++ GPTpackages/LocalModel.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 from GPTpackages.PromptTemplate import PromptTemplate
+from vllm import LLM, SamplingParams
 
 class local_LLM():
     def __init__(self,prompt:str,model:str="yentinglin/Llama-3-Taiwan-8B-Instruct-awq",
@@ -10,13 +11,13 @@ class local_LLM():
             api_key=openai_api_key,
             base_url=openai_api_base,
         )
+        self.offline_model=LLM(model="yentinglin/Llama-3-Taiwan-8B-Instruct-awq",quantization='AWQ', dtype="float16")
         self.model=model
         self.prompt=PromptTemplate(prompt)
         self.temperature=temperature
         self.img_stm =img_memory
-
-    def run(self, text_dict: dict, img_list=[],img_refresh=False) -> str:
-        send = []
+    def load_img(self,img_list=[],img_refresh=False)->list[dict]:
+        send=[]
         # load img
         if self.img_stm != None:
             if img_list != []:
@@ -48,6 +49,28 @@ class local_LLM():
                         "url": f"data:image/jpeg;base64,{img}",
                         "detail": "low"
                 }})
+        return send
+    def run_offline(self,text_dict: dict, img_list=[],img_refresh=False) -> str:
+        send=self.load_img(img_list=[],img_refresh=False)
+        text = self.prompt.format(text_dict)  
+        print("==================================\n"+text+"\n====================================")
+        send.append({
+            "type": "text",
+            "text": text
+        })
+        dev_prompt=self.prompt.get_dev_prompt()
+        message = [
+            {"role": "system","content": dev_prompt},
+            {"role": "user","content": send}
+            ]
+        sampling_params = SamplingParams(temperature=0.8, top_p=0.65,max_tokens=1024)#max tokens要手動調整，default只有32
+        outputs = self.offline_model.chat(message, sampling_params)
+
+        generated_text = outputs[0].outputs[0].text
+        return generated_text
+        
+    def run_online(self, text_dict: dict, img_list=[],img_refresh=False) -> str:
+        send = self.load_img(img_list=[],img_refresh=False)
                     
         # load text 
         text = self.prompt.format(text_dict)  
@@ -60,7 +83,7 @@ class local_LLM():
         # form request
         dev_prompt=self.prompt.get_dev_prompt()
         message = [
-            {"role": "developer","content": dev_prompt},
+            {"role": "system","content": dev_prompt},
             {"role": "user","content": send}
             ]
 
